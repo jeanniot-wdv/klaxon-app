0512e13dccfb7e86c4ccd098400d8a3db450919c
Romain Jeanniot <jeanniot.webdev@icloud.com>
2025-10-30 17:50:58 +0100

    Correction bug Router

--- core/Router.php
+++ core/Router.php
@@ -6,23 +6,32 @@ class Router
 {
   private $routes = [];
 
-  public function add(string $path, string $controller, string $method): void
+  public function add(string $path, string $controller, string $method, array $httpMethods = ['GET']): void
   {
-    $this->routes[$path] = [
-      'controller' => $controller,
-      'method' => $method,
-      'path' => $path  // Stocke le chemin original pour la génération d'URLs
-    ];
+    if (!isset($this->routes[$path])) {
+      $this->routes[$path] = [];
+    }
+    foreach ($httpMethods as $httpMethod) {
+      $this->routes[$path][$httpMethod] = [
+        'controller' => $controller,
+        'method' => $method,
+        'path' => $path
+      ];
+    }
   }
 
   public function match(string $url): array
   {
-    foreach ($this->routes as $path => $route) {
+    $requestMethod = $_SERVER['REQUEST_METHOD'] ?? 'GET';
+
+    foreach ($this->routes as $path => $methods) {
       // Convertit /agences/{id} en une regex comme #^/agences/(\d+)$#
       $pattern = preg_replace('/\{([a-z]+)\}/', '(?P<$1>[^\/]+)', $path);
       $regex = "@^" . $pattern . "$@";
 
       if (preg_match($regex, $url, $matches)) {
+        if (isset($methods[$requestMethod])) {
+          $route = $methods[$requestMethod];
         // Filtre les paramètres nommés (ex: "id" => "5")
         $params = array_filter($matches, 'is_string', ARRAY_FILTER_USE_KEY);
         return [
@@ -31,6 +40,7 @@ class Router
           'params' => array_values($params)  // Convertit en tableau indexé [0 => "5"]
         ];
       }
+    }
     }
     throw new \Exception("Route non trouvée: " . $url);
   }
