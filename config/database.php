c1b48a8f550f97967d1687f9207eb90a200218ea
Romain Jeanniot <jeanniot.webdev@icloud.com>
2025-10-30 19:43:45 +0100

    Ajout Readme

--- config/database.php
+++ config/database.php
@@ -1,8 +1,8 @@
 <?php
 return [
-    'host' => '127.0.0.1:8889',
+    'host' => '127.0.0.1:8889', // Ajustez selon votre configuration locale
     'dbname' => 'klaxon-app',
-    'username' => 'root',
-    'password' => 'root',
+    'username' => 'root', // Ajustez selon votre configuration locale
+    'password' => 'root', // Ajustez selon votre configuration locale
     'charset' => 'utf8mb4'
 ];
